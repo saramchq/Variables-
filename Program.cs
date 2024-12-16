ef09348f258b904ce05b30ed36df4c5e4cda4401
Sara <Sara@DESKTOP-R2BTG91>
2024-12-16 18:08:24 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace variaveis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Olá, utilizador! Escreva um número qualquer");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numero + 5);
+
+            int numA, numB;
+            Console.WriteLine("Olá usuário! Digite um número qualquer");
+            numA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Por favor digite mais um número!");
+            numB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A soma de {0} com {1} é igual a {2}", numA, numB, (numA + numB));
+            Console.ReadKey();
+        }
+    }
+}   
